d4d52cf1854f5a45257933a3ffa6186c52a6e722
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-22 23:34:20 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+     const routes = [
+          { "id": 1, "name": "Home", "path": "/" },
+          { "id": 2, "name": "About", "path": "/about" },
+          { "id": 3, "name": "Contact", "path": "/contact" },
+          { "id": 4, "name": "Services", "path": "/services" },
+          { "id": 5, "name": "Blog", "path": "/blog" }
+     ];
+        
+
+     return (
+          <nav>
+               <ul>
+                    {
+                         routes.map(route => <li>{route.name}</li>)
+                    }
+               </ul>
+          </nav>
+     );
+};
+
+export default Navbar;
