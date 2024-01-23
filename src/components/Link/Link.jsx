ed4d1de0d8d59306ee8b494cc099bb3e89a5bab6
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-23 20:56:24 +0600

    set menu responsive

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = ({route}) => {
      return (
-          <li className='mr-20'>
+          <li className='mr-20 rounded font-semibold hover:bg-amber-500'>
                <a href={route.path}>{route.name}</a>
           </li>
      );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,11 +15,21 @@ const Navbar = () => {
 
      return (
           <nav>
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" onClick={() => setOpen(!open)} className="w-6 h-6 cursor-pointer">
-               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-               </svg>
-
-               <ul className='md:flex'>
+               <div onClick={() => setOpen(!open)} className='md:hidden '>
+                    <span>
+                         {
+                              open === true ? 
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                              </svg>
+                              :
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                              </svg>
+                         }
+                    </span>
+               </div>
+               <ul className={`md:flex absolute md:static duration-300 pl-7 py-2 rounded-md bg-amber-300 ${open? 'top-14': '-top-36'}`}>
                     {
                          routes.map(route => <Link
                               key={route.id}
