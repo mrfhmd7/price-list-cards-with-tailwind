75a985abbaac575fdd9a61e446cdd6d4d2a335ad
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-24 23:08:26 +0600

    Price Card added

---
+++ src/components/PriceCard/PriceCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PriceCard = () => {
+     return (
+          <div>
+               <h2>This is price Card.</h2>
+          </div>
+     );
+};
+
+export default PriceCard;
--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PriceCard from '../PriceCard/PriceCard';
 
 const PriceList = () => {
+     const [prices, setPrices] = useState([]);
+
+     useEffect(() => {
+          fetch('prices.json')
+               .then(res => res.json())
+               .then(data => setPrices(data))
+     },[])
      return (
           <div>
                <h2 className='text-5xl text-center text-cyan-900 bg-cyan-300 font-bold p-4'>This is Price list.</h2>
+               <PriceCard></PriceCard>
           </div>
      );
 };
