7d0410deb769ef1ec05bfeb71d774c370e5746f0
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-24 23:03:29 +0600

    json file added

---
+++ src/components/PriceList/PriceList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PriceList = () => {
+     return (
+          <div>
+               <h2 className='text-5xl text-center text-cyan-900 bg-cyan-300 font-bold p-4'>This is Price list.</h2>
+          </div>
+     );
+};
+
+export default PriceList;
