11f8bcb709754d0b6f99b832817d59489996c0f8
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-25 22:16:55 +0600

    shown features

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const PriceCard = ({ price }) => {
      return (
-          <div>
-               <h3 className='text-5xl font-bold text-purple-700'>{price.price}</h3>
-               <h4 className='text-2xl font-bold'>{price.name}</h4>
+          <div className='bg-emerald-400 mt-8 rounded-md'>
+               <div className='text-center p-4'>
+                    <h3 className='text-5xl font-bold text-purple-700'>{price.price}</h3>
+                    <h4 className='text-3xl font-bold'>{price.name}</h4>
+               </div>
+               <div className='p-3 text-xl'>
+                    <p>Features:</p>
+                    {
+                         price.features.map(feature => <li>{feature}</li>)
+                    }
+               </div>
           </div>
      );
 };
